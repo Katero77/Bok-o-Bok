33a729ff6722397b2d618784b27e2d9528e8e570
katero77 <katebondarenkozp@gmail.com>
2021-10-16 22:25:03 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>БОК о БОК</title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="icon" href="img/benefits-icon1.svg">
+   </head>
+   <body>
+      <div class="wrapper">
+         <div class="hero">
+            <div class="container">
+               <div class="header">
+                  <div class="logo">
+                     <img src="img/logo.png" alt="">
+                  </div>
+                  <ul class="navigation">
+                     <li>
+                        <a>Преимущества</a>
+                     </li>
+                     <li>
+                        <a>Шаги продвижения</a>
+                     </li>
+                     <li>
+                        <a>Цена</a>
+                     </li>
+                  </ul>
+               </div>
+               <div class="cta">
+                  <img src="img/hero-illustration.png" alt="" class="mob hero-illustration">
+                  <h1>
+                     Станьте <span>ближе</span> к своим клиентам
+                  </h1>
+                  <p class="subtitle">
+                     Мы помогаем бизнесу получать лояльных клиентов и увеличивать объемы продаж
+                  </p>
+                  <a class="button">Разместить информацию о компании</a>
+               </div>
+            </div>
+         </div>
+         <div class="benefits">
+            <div class="container">
+               <h2>
+                  Что дает профиль компании на платформе <span>БОК о БОК</span> владельцам бизнеса?
+               </h2>
+               <div class="benefits-flex">
+                  <div class="item">
+                     <img src="img/benefits-icon1.svg" alt="">
+                     <p>
+                        Повышение рейтинга компании среди конкурентов в онлайне
+                     </p>
+                  </div>
+                  <div class="item">
+                     <img src="img/benefits-icon2.svg" alt="">
+                     <p>
+                        Оптимизацию работы с лидерами мнений и негативными отзывами
+                     </p>
+                  </div>
+                  <div class="item">
+                     <img src="img/benefits-icon3.svg" alt="">
+                     <p>
+                        Увеличение уровня доверия среди реальных и потенциальных клиентов
+                     </p>
+                  </div>
+                  <div class="item">
+                     <img src="img/benefits-icon4.svg" alt="">
+                     <p>
+                        Расширение рынка сбыта продукции или зоны предоставления услуг
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="info">
+            <div class="container">
+               <img src="img/info-illustration.png" alt="" class="mob info-illustration">
+               <h2>
+                  <span>БОК о БОК</span> — удобный трамплин для роста вашего бизнеса
+               </h2>
+            </div>
+         </div>
+         <div class="steps">
+            <div class="container">
+               <h2>
+                  3 шага для последовательного продвижения
+               </h2>
+               <div class="steps-list">
+                  <div class="step">
+                     <div class="step-num">
+                        01
+                     </div>
+                     <div class="step-text">
+                        <h3>
+                           Создайте профиль компании
+                        </h3>
+                        <p>
+                           Функционал платформы позволяет не только подробно рассказывать о своем торговом предложении, но и обрабатывать запросы потенциальных клиентов. Создание профиля абсолютно бесплатно и не требует больших временных затрат
+                        </p>
+                     </div>
+                  </div>
+                  <div class="step">
+                     <div class="step-num">
+                        02
+                     </div>
+                     <div class="step-text">
+                        <h3>
+                           Работайте с мнениями аудитории
+                        </h3>
+                        <p>
+                           Так как ресурс БОК о БОК аккумулирует отзывы о любом потребительском опыте в одном месте, у вас есть уникальная возможность работать с мнением своей аудитории, не выходя за пределы площадки
+                        </p>
+                     </div>
+                  </div>
+                  <div class="step">
+                     <div class="step-num">
+                        03
+                     </div>
+                     <div class="step-text">
+                        <h3>
+                           Используйте преимущество рекламы
+                        </h3>
+                        <p>
+                           Каждый день ресурс БОК о БОК собирает множество пользователей, которые хотят узнать больше о том или ином продукте. Распространение информации о себе в такой среде помогает бизнесу быстро привлекать новых клиентов
+                        </p>
+                     </div>
+                  </div>
+               </div>
+               <a class="button">Создать профиль компании</a>
+            </div>
+         </div>
+         <div class="cards">
+            <div class="container">
+               <div class="cards-title">
+                  <h2>
+                     Управляйте репутацией своего бизнеса
+                  </h2>
+                  <p>
+                     Отзывы и мнения о товарах или услугах напрямую влияют на эффективность бизнеса, поэтому качественное позиционирование и формирование нужного мнения целевой аудитории — это то, ради чего была создана данная платформа
+                  </p>
+               </div>
+               <div class="cards-flex">
+                  <div class="card">
+                     <img src="img/card-icon1.svg" alt="">
+                     <h3>
+                        Инициирование обратной связи с клиентом
+                     </h3>
+                     <p>
+                        Любой пользователь, оставляя мнение о товаре или сервисе, рассчитывает на ту или иную реакцию компании. Грамотная работа с отзывами позволяет улучшить потребительский опыт клиента и, как следствие, повысить уровень доверия бизнесу
+                     </p>
+                  </div>
+                  <div class="card mob-3">
+                     <img src="img/card-icon2.svg" alt="">
+                     <h3>
+                        Актуализация пользовательского опыта
+                     </h3>
+                     <p>
+                        Партнерский аккаунт в рамках платформы дает не только возможность общаться со своими клиентами напрямую, но и модерировать поступающие отзывы. Данная опция сокращает поток необоснованного недовольства клиентов и акцентирует внимание аудитории на положительном опыте реальных пользователей
+                     </p>
+                  </div>
+                  <div class="card">
+                     <img src="img/card-icon3.svg" alt="">
+                     <h3>
+                        Продвижение бренда внутри платформы
+                     </h3>
+                     <p>
+                        Внутренняя реклама позволяет владельцем бизнеса находить новых клиентов и партнеров непосредственно на самой платформе. Показы происходят не только на страницах со схожей тематикой, но и захватывают смежные отрасли
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="marketing">
+            <div class="container">
+               <div class="marketing-title">
+                  <h2>
+                     Продвижение бизнеса онлайн
+                  </h2>
+                  <p>
+                     БОК о БОК — это специализированная платформа для продвижения в сети и работы с отзывами клиентов
+                  </p>
+               </div>
+               <div class="marketing-flex">
+                  <div class="item">
+                     <img src="img/marketing1.png" alt="">
+                     <div class="text">
+                        <h3>
+                           Готовая SEO-оптимизация профиля
+                        </h3>
+                        <p>
+                           Страницы компаний на www.bokobok.com создаются с учетом важных факторов ранжирования поисковых систем Google и Яндекс, что позволяет занимать высокие позиции в поиске и привлекать потенциальных клиентов
+                        </p>
+                     </div>
+                  </div>
+                  <div class="item">
+                     <img src="img/marketing2.png" alt="">
+                     <div class="text">
+                        <h3>
+                           Удобные инструменты для маркетинга
+                        </h3>
+                        <p>
+                           Кабинет владельца бизнеса располагает удобным инструментарием для позиционирования в сети, отслеживания базовых и уникальных метрик поведения аудитории, а также все необходимое для запуска рекламных кампаний, включая email-рассылку
+                        </p>
+                     </div>
+                  </div>
+                  <div class="item">
+                     <img src="img/marketing3.png" alt="">
+                     <div class="text">
+                        <h3>
+                           Участие в партнерских программах
+                        </h3>
+                        <p>
+                           Подключение к партнерской программе предполагает дополнительный канал для продвижения бизнеса, ведь тогда владелец компании может запускать рекламный контент не только в рамках ресурса БОК о БОК, но и на специализированных партнерских площадках
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="package">
+            <div class="container">
+               <div class="package-title">
+                  <h2>
+                     Выберите тарифный план для эффективного продвижения в сети
+                  </h2>
+                  <p>
+                     Набор маркетинговых бизнес-инструментов позволит настроить оптимальную стратегию раскрутки компании внутри платформы
+                  </p>
+               </div>
+               <div class="package-flex">
+                  <div class="item">
+                     <h2>
+                        Базовый
+                     </h2>
+                     <div class="price">
+                        <span>$0</span>/месяц*
+                     </div>
+                     <ul class="package-list">
+                        <li>
+                           <p>Размещение информации о бизнесе</p>
+                        </li>
+                        <li class="disabled">
+                           <p>Коммуникация от лица компании</p>
+                        </li>
+                        <li class="disabled">
+                           <p>Модерация клиентских отзывов</p>
+                        </li>
+                        <li class="disabled">
+                           <p>Рекламное продвижение</p>
+                        </li>
+                        <li class="disabled">
+                           <p>Круглосуточная техподдержка</p>
+                        </li>
+                     </ul>
+                     <a class="button">Подключить</a>
+                  </div>
+                  <div class="item">
+                     <h2>
+                        Партнерский
+                     </h2>
+                     <div class="price">
+                        <span>$50</span>/месяц*
+                     </div>
+                     <ul class="package-list">
+                        <li>
+                           <p>Размещение информации о бизнесе</p>
+                        </li>
+                        <li>
+                           <p>Коммуникация от лица компании</p>
+                        </li>
+                        <li>
+                           <p>Модерация клиентских отзывов</p>
+                        </li>
+                        <li>
+                           <p>Рекламное продвижение</p>
+                        </li>
+                        <li>
+                           <p>Круглосуточная техподдержка</p>
+                        </li>
+                     </ul>
+                     <a class="button">Подключить</a>
+                  </div>
+               </div>
+               <p>
+                  *Тарифный план можно изменить в любое время
+               </p>
+            </div>
+         </div>
+         <div class="feedback">
+            <div class="container">
+               <h2>
+                  Задайте нам вопрос о размещении компании в каталоге
+               </h2>
+               <div class="form">
+                  <img src="img/form-illustration.png" alt="">
+                  <form >
+                     <label for="name">
+                        <p>Ваше имя</p>
+                     </label>
+                     <input type="text" name="name" id="name" placeholder="Введите ваше имя" required="">
+                     <label for="tg">
+                        <p>Ваш Telegram</p>
+                     </label>
+                     <input type="text" name="tg" id="tg" placeholder="@nickname" required="">
+                     <label for="email">
+                        <p>Ваша электронная почта</p>
+                     </label>
+                     <input type="email" name="email" id="email" placeholder="you@domain.com" required="">
+                     <label for="ques">
+                        <p>Ваш вопрос</p>
+                     </label>
+                     <input type="text" name="ques" id="ques" placeholder="Сформулируйте ваш вопрос" required="">
+                     <button class="button">Отправить</button>
+                     <p>
+                        Нажатием на кнопку вы даете согласие на <span>обработку персональных данных</span>
+                     </p>
+                  </form>
+               </div>
+            </div>
+         </div>
+         <footer>
+            <div class="container">
+               <div class="top-line">
+                  <img src="img/logo.png" alt="" class="footer-logo">
+                  <div class="column">
+                     <a>
+                        <p>Преимущества</p>
+                     </a>
+                     <a>
+                        <p>Шаги продвижения</p>
+                     </a>
+                     <a>
+                        <p>Цена</p>
+                     </a>
+                  </div>
+                  <div class="column">
+                     <a>
+                        <p>Форма обратной связи</p>
+                     </a>
+                     <a>
+                        <p>Создать профиль компании</p>
+                     </a>
+                  </div>
+                  <div class="social">
+                     <a>
+                     <img src="img/fb.svg" alt="">
+                     </a>
+                     <a>
+                     <img src="img/inst.svg" alt="">
+                     </a>
+                     <a>
+                     <img src="img/linkedin.svg" alt="">
+                     </a>
+                  </div>
+               </div>
+               <div class="bot-line">
+                  <a>
+                     <p>Правила использования</p>
+                  </a>
+                  <a>
+                     <p>Политика конфиденциальности</p>
+                  </a>
+               </div>
+            </div>
+         </footer>
+      </div>
+   </body>
+</html>
